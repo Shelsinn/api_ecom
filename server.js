c45112a9e60fcddb92d73dad36c5c004d2075ebe
Shelsinn <vincentfourcroy@hotmail.fr>
2024-01-08 11:53:44 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Chargement des variables d'environnement.
+// Import des modules nécessaires.
+// Import des routes pour l'authentification.
+// Import de la configuration de la BDD.
+// Initialisation de l'application Express.
+// Middleware pour traiter les requêtes JSON.
+// Middleware pour parser les cors de requêtes.
+// Utilisation des routes pour l'authentification.
+// Configuration des options cors.
+// Middleware pour gérer les cors.
+// Définition du port de démarrage du serveur.
+// Fonction pour démarrer le serveur.
+// Appel de la fonction pour démarrer le serveur.
