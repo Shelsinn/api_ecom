02670e628658376b2361e9bd07631eabf4088ff7
Shelsinn <vincentfourcroy@hotmail.fr>
2024-01-09 11:05:48 +0100

    created dockerfile and docker-compose

--- models/auth.model.js
+++ models/auth.model.js
@@ -47,7 +47,7 @@ userSchema.pre("save", async function (next) {
   try {
     if (!this.isModified("password")) {
       return next();
-    }
+    };
     const hashedPassword = await bcrypt.hash(this.password, 10);
     this.password = hashedPassword;
     return next();
